9af1c932404a4527d0da44e43e2f3490fe64a67e
ZmixZ <termixator@yandex.ru>
2020-10-06 16:44:18 +0300

    First homework

---
+++ src/test/java/openYa.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class openYa {
+        private WebDriver driver;
+        private String baseUrl;
+
+        @BeforeClass(alwaysRun = true)
+        public void setUp() throws Exception {
+            driver = new ChromeDriver();
+            baseUrl = "https://www.google.com/";
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        }
+
+        @Test
+        public void openYa() throws Exception {
+            driver.get("https://yandex.ru/");
+        }
+
+        @AfterClass(alwaysRun = true)
+        public void tearDown() throws Exception {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+
